79341080a8c6ccd1b579fd29f4f96e9f865b99df
robert <robert89221@outlook.com>
2023-11-15 14:54:42 +0100

    Add office and local currency to the model

--- AssetTracking/Asset.cs
+++ AssetTracking/Asset.cs
@@ -1,16 +1,46 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace AssetTracking
 {
     internal class Asset
     {
+        public enum Office
+        {
+            Malmö,
+            Tokyo,
+            SãoPaulo
+        }
+
         public string Type { get; protected set; }
         public string Brand { get; protected set; }
         public string Model { get; protected set; }
         public DateTime PurchaseDate { get; protected set; }
         public int Price { get; protected set; }
+        public Office Location;
 
-        public Asset(string t, string b, string m, DateTime d, int p)  =>  (Type, Brand, Model, PurchaseDate, Price) = (t, b, m, d, p);
+        public Asset(string t, string b, string m, string l, DateTime d, int p)
+        {
+            Office o = 0;
 
-        override public string ToString() => $"{Type,-12}{Brand,-12}{Model,-12}{PurchaseDate.ToShortDateString(),12}{Price,10}";
+            l = l.ToUpper();
+            if (l == "M")         o = Office.Malmö;
+            else if (l == "T")    o = Office.Tokyo;
+            else if (l == "S")    o = Office.SãoPaulo;
+
+            (Type, Brand, Model, Location, PurchaseDate, Price) = (t, b, m, o, d, p);
+        }
+
+        override public string ToString()
+        {
+            double localPrice = 0.0;
+            string currency = "";
+
+            if (Location == Office.Malmö)            (localPrice, currency) = (Price * 10.5, "SEK");
+            else if (Location == Office.Tokyo)       (localPrice, currency) = (Price * 150.66, "YEN");
+            else if (Location == Office.SãoPaulo)    (localPrice, currency) = (Price * 4.86, "BRL");
+
+            return $"{Type,-12}{Brand,-12}{Model,-15}{Location.ToString(),-10}{PurchaseDate.ToShortDateString(),12}{Price,10}{localPrice,15:0.00}{currency,4}";
+        }
     }
 }
--- AssetTracking/Program.cs
+++ AssetTracking/Program.cs
@@ -9,10 +9,10 @@ var assets = new List<Asset>();
 PopulateInventory();
 
 
-Console.WriteLine("Type        Brand       Model         Date           Price\n");
+Console.WriteLine("Type        Brand       Model          Office      Date       Price USD        Local price\n");
 
 var query = from Asset item in assets
-            orderby item.Type, item.PurchaseDate
+            orderby item.Location.ToString(), item.PurchaseDate
             select item;
 
 foreach (var item in query)
@@ -29,13 +29,19 @@ foreach (var item in query)
 
 void PopulateInventory()
 {
-    assets.Add(new Asset("Computer", "Lenovo", "L23", new DateTime(2021, 4, 11), 2000));
-    assets.Add(new Asset("Phone", "Xiaomi", "X12", new DateTime(2022, 5, 6), 245));
-    assets.Add(new Asset("Tablet", "Samsung", "Pad P4", new DateTime(2023, 8, 7), 810));
-    assets.Add(new Asset("Computer", "Siemens", "Power W5", new DateTime(2020, 2, 12), 3000));
-    assets.Add(new Asset("Phone", "Nokia", "3.1", new DateTime(2020, 8, 4), 450));
-    assets.Add(new Asset("Tablet", "Sony", "SonyPad II", new DateTime(2020, 9, 20), 700));
-    assets.Add(new Asset("Computer", "Asus", "Super", new DateTime(2010, 2, 1), 800));
-    assets.Add(new Asset("Phone", "Sharp", "Aquos 4", new DateTime(2021, 6, 18), 900));
-    assets.Add(new Asset("Tablet", "Huawei", "WordPad 2", new DateTime(2021, 5, 1), 552));
+    assets.Add(new Asset("Computer", "Lenovo",  "L23",        "M", new DateTime(2021, 4, 11),  2100));
+    assets.Add(new Asset("Phone",    "Xiaomi",  "X12",        "T", new DateTime(2022, 5, 6),    245));
+    assets.Add(new Asset("Tablet",   "Samsung", "Pad P4",     "T", new DateTime(2023, 8, 7),    810));
+    assets.Add(new Asset("Computer", "Siemens", "Power W5",   "M", new DateTime(2020, 2, 12),  2995));
+    assets.Add(new Asset("Phone",    "Nokia",   "3.1",        "M", new DateTime(2020, 8, 4),    450));
+    assets.Add(new Asset("Tablet",   "Sony",    "SonyPad II", "S", new DateTime(2020, 9, 20),   700));
+    assets.Add(new Asset("Computer", "Asus",    "Super",      "S", new DateTime(2010, 2, 1),   1900));
+    assets.Add(new Asset("Phone",    "Sharp",   "Aquos 4",    "S", new DateTime(2021, 6, 18),   900));
+    assets.Add(new Asset("Tablet",   "Huawei",  "WordPad 2",  "T", new DateTime(2021, 5, 1),    355));
+    assets.Add(new Asset("Tablet",   "Asus",    "MatePad X",  "M", new DateTime(2022, 11, 5),   450));
+    assets.Add(new Asset("Tablet",   "Sony",    "Padio III",  "M", new DateTime(2023, 1, 9),    595));
+    assets.Add(new Asset("Phone",    "Asus",    "Zenfone 2",  "S", new DateTime(2022, 9, 28),   715));
+    assets.Add(new Asset("Phone",    "Nokia",   "X20",        "S", new DateTime(2023, 3, 17),   550));
+    assets.Add(new Asset("Computer", "Toshiba", "T40",        "T", new DateTime(2022, 4, 30),  1410));
+    assets.Add(new Asset("Computer", "Fujitsu", "FJ-5000",    "T", new DateTime(2022, 12, 24), 3510));
 }
